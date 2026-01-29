002d4e4378170bb377b915e014393253c53c8078
Pramit <pramitmanna19@gmail.com>
2026-01-29 14:33:40 +0530

    code: setup

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,6 +1,20 @@
 import connectDB from "@airmeet/database";
 import { User } from "@airmeet/models";
+import express from "express";
+import cors from "cors";
 
-const PORT = Number(process.env.PORT) || 3000
+
+const PORT = Number(process.env.PORT) || 5000
 connectDB(process.env.MONGO_URI!);
+const app = express();
+app.use(cors(
+    {
+        origin: "*",
+    }
+));
 
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
+})
